ebd25a47dd00c2efc5d43142e94c37969cf40135
[angela riggs] <[riggs.ang@gmail.com]>
2015-05-11 20:21:04 -0700

    added descriptions for each room; added var functions to start game

--- play2.js
+++ play2.js
@@ -1,6 +1,6 @@
 var draw = require('jsi-gamelib').map;
 
-var map = require('./map.json')
+var map = require('./map.json');
 
 draw(map);
 
@@ -11,9 +11,41 @@ var rl = readline.createInterface({
 	output: process.stdout
 });
 
+var dictionary = {A:0, B:1, C:2, D:3, E:4, F:5, G:6, H:7, I:8, J:9, K:10}
+
 
 var currentRoom = "A";
 
-if (currentRoom === "A") {
-	console.log(map.rooms[0].name)
-}
+// var describeRoom = function() {
+// 	console.log(map.rooms[dictionary[currentRoom]].description)
+// }
+
+var describeRoom = function() {
+	for (var i = 0; i < map.rooms.length; i++) {
+		if (map.rooms[i].name === currentRoom) {
+			console.log(map.rooms[dictionary[currentRoom]].description);
+		}
+	}
+};
+
+describeRoom();
+
+
+// for (var i = 0; i < map.rooms.length; i++) {
+// 	if(map.rooms[i].name === currentRoom) {
+// 		rl.question(map.rooms[i].description, function(answer){
+// 		});
+// 	};
+// };
+
+// if (currentRoom === "A") {
+// 	rl.question(map.rooms[dictionary.currentRoom].description, function(answer) {
+// 		if (answer.toLowerCase() === "north") {
+// 			currentRoom === "D";
+// 			console.log("Room D");
+// 		} else if (answer.toLowerCase === "east", function(answer) {
+// 			currentRoom === "B";
+// 			console.log("Room B")
+// 		});
+// 	});
+// }
